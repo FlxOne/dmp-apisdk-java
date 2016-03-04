5d443d112bd8a87226d2fb2284fb493a31793ec8
rv186025 <reinoud.vansanten@teradata.com>
2016-03-04 15:01:26 +0100

    Implemented Parameter handling in Request

--- src/main/java/request/AbstractRequest.java
+++ src/main/java/request/AbstractRequest.java
@@ -1,4 +1,19 @@
 package request;
 
+import java.util.HashMap;
+
 public class AbstractRequest implements IRequest {
+    protected HashMap<String, String> parameters = new HashMap<String, String>();
+
+    public HashMap<String, String> getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(HashMap<String, String> parameters) {
+        this.parameters.putAll(parameters);
+    }
+
+    public void setParameter(String key, String value) {
+        parameters.put(key, value);
+    }
 }
--- src/main/java/request/IRequest.java
+++ src/main/java/request/IRequest.java
@@ -1,4 +1,9 @@
 package request;
 
+import java.util.HashMap;
+
 public interface IRequest {
+    void setParameter(String key, String value);
+    void setParameters(HashMap<String, String> parameters);
+    HashMap<String, String> getParameters();
 }
