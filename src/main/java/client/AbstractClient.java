6586c91bda44461dbec56cf8bbccdb89213523be
rv186025 <reinoud.vansanten@teradata.com>
2016-03-04 15:22:18 +0100

    Moved constructor to abstract class

--- src/main/java/client/AbstractClient.java
+++ src/main/java/client/AbstractClient.java
@@ -2,10 +2,19 @@ package client;
 
 import config.Config;
 import config.IConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import request.IRequest;
 import response.IResponse;
 
 public abstract class AbstractClient implements IClient {
+    protected CloseableHttpClient client;
+    protected IConfig config;
+
+    public AbstractClient(IConfig config) {
+        this.config = config;
+        this.client = HttpClients.createDefault();
+    }
 
     public IResponse get(IRequest request) {
         return null;
--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,15 +1,9 @@
 package client;
 
 import config.IConfig;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 
 public class Client extends AbstractClient implements IClient {
-    protected IConfig config;
-    protected CloseableHttpClient client;
-
     public Client(IConfig config) {
-        this.config = config;
-        this.client = HttpClients.createDefault();
+        super(config);
     }
 }
