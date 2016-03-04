cf389c1a2336abf1c6a11b17190c88d573223f4a
rv186025 <reinoud.vansanten@teradata.com>
2016-03-04 15:18:43 +0100

    Added apache http client dependency

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,11 +1,15 @@
 package client;
 
 import config.IConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 
 public class Client extends AbstractClient implements IClient {
     protected IConfig config;
+    protected CloseableHttpClient client;
 
     public Client(IConfig config) {
         this.config = config;
+        this.client = HttpClients.createDefault();
     }
 }
