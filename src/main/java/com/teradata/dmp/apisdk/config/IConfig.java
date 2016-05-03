c20c5618cf831f89d6cb0f5a311149355462fdd1
ni186003 <nune.isabekyan@teradata.com>
2016-05-03 18:00:14 +0200

    Add HMAC header check if enabled in configuration

--- src/main/java/com/teradata/dmp/apisdk/config/IConfig.java
+++ src/main/java/com/teradata/dmp/apisdk/config/IConfig.java
@@ -8,4 +8,8 @@ public interface  IConfig {
     void setEndpoint(String endpoint);
     void setCredentials(String user, String password);
     void setMaxRetries(int n);
+    String getHMACSecret();
+    boolean isHMACEnabled();
+    void setHMACEnabled(boolean b);
+    void setHMACSecret(String secret);
 }
