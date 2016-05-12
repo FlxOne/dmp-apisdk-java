a20261c917bfcee962cfbdfe8e7d1aa727c91428
Robin Verlangen <robin+github@us2.nl>
2016-05-12 13:16:01 +0200

    Deploy

--- copy_to_repo.sh
+++ copy_to_repo.sh
@@ -2,3 +2,6 @@
 mvn clean deploy -DskipTests # this will result in an error, which is fine..
 cp -rf target/mvn-repo/com/teradata/dmp/apisdk/* com/teradata/dmp/apisdk/
 git add -f com/teradata/dmp/apisdk/
+git add .
+git commit -m "Deploy"
+git push
